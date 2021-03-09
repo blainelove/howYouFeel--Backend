9ee75b447df90b44b36e4e3f62ff54c7769e0a03
Kwaku <kwakugyapong219@gmail.com>
2021-03-09 09:40:33 -0500

    Start

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,41 @@ class ListsController < ApplicationController
         @lists = List.all
         render json: @lists
     end
+
+    def show
+        @list = List.find(params[:id])
+        render json: @list
+    end
+
+
+    def create
+        @list = List.create(list_params)
+        render json: @list
+    end
+
+
+
+    def update
+        @list = List.find(params[:id])
+        @list.update(list_params)
+        render json: @list
+    end
+
+    def destroy
+        @list = List.find(params[:id])
+        @list.destroy
+        render json: @list
+    end
+
+
+
+    private
+
+    def list_params
+        # params.require(:list).permit(:description, :image, :date )
+        params.permit(:description, :image, :date )
+    end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/lists," to: "lists#index"
+  get "/lists", to: "lists#index"
+  post "/lists", to: "lists#create"
+  get  "/lists/:id", to: "lists#show"
+  patch "/lists/:id", to: "lists#update"
+  delete "/lists/:id", to: "lists#destroy"
 end
